cc07612cf1ebcafb5562482c45ef8f2657800310
Trent <mr.peschke@gmail.com>
2017-12-19 12:36:47 -0700

    finished

--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+require('dotenv').config();
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter')
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use( session( {
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 3000
+    }
+}))
+app.use(createInitialSession)
+app.use((req, res, next) => {
+    if ( req.method === 'POST' || req.method === 'PUT' ) {
+        filter(req, res, next)
+    } else {
+        next()
+    }
+})
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get('/api/messages/history', mc.history)
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
